ecee20ff9406afde23db3c50ea9af2532eed9660
Guilherme Castro <guii_1@hotmail.com>
2022-08-11 18:12:32 -0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "guicastro13",
+    password: "guicastro13",
+    database: "aula_postgres",
+    define: {
+        timestamps: true,
+    },
+};
