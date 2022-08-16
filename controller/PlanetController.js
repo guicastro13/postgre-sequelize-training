0f82265b38aebeba4c93561c8cdff561e5e6adde
Guilherme Castro <guii_1@hotmail.com>
2022-08-16 19:28:02 -0300

    feat:adicionado mais dois end-points no projeto

--- controller/PlanetController.js
+++ controller/PlanetController.js
@@ -7,4 +7,27 @@ module.exports = {
     const planet = await Planet.create({ name, position });
     return res.json(planet);
   },
+  async index(req, res) {
+      const planets = await Planet.findAll();
+      return res.json(planets)
+  },
+  async put(req, res) {
+    const {name, size, position} = req.body
+    await Planet.update(
+       {name, size, position},{
+          where: {
+            id: req.params.id
+          },
+       },
+    );
+    return res.send("Planeta atualizado com sucesso!")
+  },
+  async delete(req, res) {
+    await Planet.destroy({
+        where:{
+          id: req.params.id
+        }
+    })
+    return res.send("Planeta excluido com sucesso!")
+  }
 };
