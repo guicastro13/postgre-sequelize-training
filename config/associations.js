eb5f5e998da45d803994c982d7afd4fb02642de8
Guilherme Castro <guii_1@hotmail.com>
2022-08-17 14:37:51 -0300

    feat: adicionado mais 3 tabelas manyTomany no banco

--- config/associations.js
+++ config/associations.js
@@ -1,7 +1,24 @@
 const Planet = require('../models/Planet');
 const Satellite = require('../models/Satellite'); 
 
-Planet.hasOne(Satellite,{onDelete: "CASCADE", onUpdate: "CASCADE"});
+const Cap = require('../models/Cap');
+const Spaceship = require('../models/Spaceship')
+
+// Planet.hasOne(Satellite,{onDelete: "CASCADE", onUpdate: "CASCADE"});
+// Satellite.belongsTo(Planet,{foreingKey: "planetId", as: "planet"});
+
+Planet.hasMany(Satellite,{onDelete: "CASCADE", onUpdate: "CASCADE"});
 Satellite.belongsTo(Planet,{foreingKey: "planetId", as: "planet"});
+
+Cap.belongsToMany(Spaceship, {
+    foreingKey: "capsId",
+    throught: "capSpaceship",
+    as: "spaceships"
+});
+Spaceship.belongsToMany(Cap, {
+    foreingKey: "spaceshipId",
+    throught: "capSpaceship",
+    as: "caps"
+})
 
 module.exports = {Planet, Satellite};
